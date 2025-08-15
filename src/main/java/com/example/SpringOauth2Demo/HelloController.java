ea19cc4ecb948fc40bd4085bc1be31e5f1aaf8f3
PranithMadishetti <pranithmadishetti9@gmail.com>
2025-08-15 10:35:01 -0400

    demonstraion od springoAuth2

---
+++ src/main/java/com/example/SpringOauth2Demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.SpringOauth2Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to My page";
+    }
+}
